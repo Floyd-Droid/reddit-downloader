c730abe8f39d68e047326fe049c78431225bb3bc
Jourdon <jourdonfloyd@gmail.com>
2021-03-06 19:14:02 -0500

    Validate form fields in conjunction with PRAW and PSAW capabilities

--- downloader/forms.py
+++ downloader/forms.py
@@ -12,3 +12,60 @@ class SearchForm(forms.ModelForm):
             'date_searched',
             'user'
         )
+
+    def __init__(self, *args, **kwargs):
+        super(SearchForm, self).__init__(*args, **kwargs)
+        self.fields['start_date'].input_formats = ['%m/%d/%Y']
+        self.fields['end_date'].input_formats = ['%m/%d/%Y']
+        self.data = kwargs.get('data')
+
+    def clean_subreddit(self):
+        """Format the subreddit string: lowercase and remove whitespace."""
+        cleaned_data = super(SearchForm, self).clean()
+        sub = cleaned_data.get('subreddit')
+        # If no subreddit is given, return 'all'
+        if not sub:
+            sub = 'all'
+        else:
+            sub = "".join(sub.split()).lower()
+
+        return sub
+    
+    def clean_limit(self):
+        cleaned_data = super(SearchForm, self).clean()
+        lim = cleaned_data.get("limit")
+        time_option = self.data.get("time_option")
+        if time_option == 'time_filter' and lim > 500:
+            self.add_error('limit', 'To use the standard Reddit time filter, the limit must be no greater than 500')
+        elif time_option == 'date_range' and lim > 5000:
+            self.add_error('limit', 'Please limit to no more than 5,000 results')
+        
+        return lim
+
+    def clean(self):
+        cleaned_data = super(SearchForm, self).clean()
+        praw_sort = cleaned_data.get('praw_sort')
+        subreddit_str = cleaned_data.get('subreddit')
+        subreddit_list = subreddit_str.split(',')
+        if self.data.get("time_option") == 'time_filter':
+            # Date range options are excluded
+            cleaned_data['start_date'] = cleaned_data['end_date'] = cleaned_data['psaw_sort'] = None
+
+            # Search terms are not allowed for front page search or for certain praw sort options.
+            if praw_sort in ['controversial', 'rising', 'random_rising'] or 'front' in subreddit_list:
+                cleaned_data['terms'] = None
+            # A time filter won't apply to certain praw sorts.
+            if praw_sort in ['hot', 'new', 'rising', 'random_rising']:
+                cleaned_data['time_filter'] = None
+            # The following praw sort options require search terms
+            if praw_sort in ['relevance', 'comments'] and not cleaned_data['terms']:
+                self.add_error('terms', 'Search terms are required for the selected sort option')
+
+        elif self.data.get("time_option") == 'date_range':
+            cleaned_data['time_filter'] = cleaned_data['praw_sort'] = None
+
+            # front page results not possible for psaw. Remove from subreddit field.
+            if 'front' in subreddit_list:
+                self.add_error('subreddit', "Please either remove 'front' or select the time filter option instead of the date range.")
+
+        return cleaned_data
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
-  $("table.table").DataTable({
-    // Do not allow default sorting (on the first column)
+  let subTable = $("table.table").DataTable({
+    // Do not allow default sorting
     "order":[]
+  });
+
+  // Inform the user that their search terms will be ignored if
+  // 'controversial', 'rising', or 'random rising' is selected in praw_sort
+  $("#id_praw_sort").change(function () {
+    let p = this.parentNode;
+    let options = ["controversial", "rising", "random_rising"];
+    if (options.includes(this.value)) {
+      if (!p.innerHTML.includes("<span")) {
+        $("<span id='warning_message'>Search terms will be ignored for this option</span>").appendTo(p);
+      }
+    } else {
+      // Remove warning message if a viable option is selected
+      if (p.innerHTML.includes("<span")) {
+        $("#warning_message").remove();
+      }
+    }
   });
 })

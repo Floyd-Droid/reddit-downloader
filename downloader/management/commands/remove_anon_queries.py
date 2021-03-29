a1cb046cf401c067cfe9acf222d67a8d30e825de
Jourdon <jourdonfloyd@gmail.com>
2021-03-29 16:10:56 -0400

    Add class to periodically remove anonymous query objects

---
+++ downloader/management/commands/remove_anon_queries.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from .models import SearchQuery
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    help = 'Deletes SearchQuery objects created by anonymous users which are older than one day.'
+
+    def handle(self):
+        try:
+            SearchQuery.objects.filter(user=None, date_created__lte=datetime.now()-timedelta(days=1)).delete()
+        except:
+            self.stdout.write(self.style.ERROR('Failed to delete stale queries.'))
+            return
+
+        self.stdout.write(self.style.SUCCESS('Successfully deleted stale queries.'))
+        return
